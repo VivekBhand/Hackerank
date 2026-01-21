20c8d4713b890716524421c72bef0999efe1df30
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2026-01-21 22:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (61.17%) - LeetSync

--- 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> res;
-        for (int n : nums) {
-            if (n & 1) {
-                int z = ((n + 1) & ~n) >> 1;
-                res.push_back(n & ~z);
-            } else
-                res.push_back(-1);
+        int n = nums.size();
+        vector<int> ans(n, -1);
+
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+
+            // 2 has no solution
+            if (x == 2) {
+                ans[i] = -1;
+                continue;
+            }
+
+            // Count trailing ones
+            int t = 0;
+            while ((x >> t) & 1) {
+                t++;
+            }
+
+            // If no trailing ones (even number), no solution
+            if (t == 0) {
+                ans[i] = -1;
+                continue;
+            }
+
+            // Remove the HIGHEST trailing-one bit
+            int bit = 1 << (t - 1);
+            int candidate = x - bit;
+
+            if ((candidate | (candidate + 1)) == x) {
+                ans[i] = candidate;
+            } else {
+                ans[i] = -1;
+            }
         }
-        return res;
+
+        return ans;
     }
 };
