58139ca35b7e18ff042d68c084562f0de2f3b0b6
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2026-02-05 23:05:34 +0530

    Time: 10 ms (22.28%) | Memory: 25.9 MB (8.08%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        vector<int> ans;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int ind = i + nums[i] + 100*n;
+            ind = ind % n;
+            ans.push_back(nums[ind]);
+        }
+        return ans;
+
+    }
+};
