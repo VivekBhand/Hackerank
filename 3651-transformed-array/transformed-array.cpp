e58aca09d5c25e0d2e128ffe1265a895fc58e298
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2026-02-05 23:05:52 +0530

    Time: 12 ms (13.37%) | Memory: 25.8 MB (39.55%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            int ind = i + nums[i] + 100*n;
+            int ind = i + nums[i] + 10*n;
             ind = ind % n;
             ans.push_back(nums[ind]);
         }
