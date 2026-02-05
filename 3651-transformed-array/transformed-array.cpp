d15646d17ad07911b2c3953a3501c4c4fda5e122
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2026-02-05 23:06:55 +0530

    Time: 3 ms (89.69%) | Memory: 25.6 MB (70.75%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
-        vector<int> ans;
 
         int n = nums.size();
+        vector<int> ans(n, 0);
         int ind;
         for (int i = 0; i < n; ++i) {
             ind = i + nums[i] + 10*n;
             ind = ind % n;
-            ans.push_back(nums[ind]);
+            ans[i] = nums[ind];
         }
         return ans;
 
