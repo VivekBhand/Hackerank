35f3a05bac4050a835ab4af63e08aabc5fc1bf83
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2026-02-24 23:47:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (53.22%) - LeetSync

---
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int current) {
+        if (root == nullptr) {
+            return 0;
+        }
+        current = current * 2 + root-> val;
+
+        if (root->left == nullptr && root->right == nullptr) {
+            return current;
+        }
+        return dfs(root->left, current) + dfs(root->right, current);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+};
